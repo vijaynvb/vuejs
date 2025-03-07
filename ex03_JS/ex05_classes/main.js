1587fc29ccdd96b6980620a16b2ef86c8d363136
vijay <vijay.nvb@gmail.com>
2025-03-07 12:33:57 +0530

    classes es6 and es5

---
+++ ex03_JS/ex05_classes/main.js
@@ -0,0 +1,31 @@
+// es 6 class 
+
+class Person{
+    //name="vijay";
+    //age =10;
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    print(){
+        console.log("hello");
+    }
+}
+
+var p = new Person("vijay",10);
+console.log(typeof p);
+
+
+// es5 
+
+function personfun(name, age){
+    this.name = name;
+    this.age = age;
+}
+
+personfun.prototype.print = function(){
+    console.log("hello");
+}
+
+var fun = new personfun("vijay", 10);
+console.log(typeof fun);
